cc7c107de5d6b10f1d29f67edf35ef0f9dfb7489
LouisChang <changsidi@gmail.com>
2020-10-02 22:53:26 -0700

    V1
    Refactor some code some comments.

---
+++ gross_forecasting/inflation.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+def adjust_monetization(time: pd.Series, monetization: pd.Series, cpi_df: pd.DataFrame):
+    """ Adjust based on Aug 2020 data."""
+    cpi_aug = cpi_df.loc[pd.to_datetime('2020-08-01')]['Value']
+    combined_df = pd.concat([time, monetization], axis=1)
+
+    return combined_df.apply(lambda t: int(cpi_aug / cpi_df.loc[t[0]]['Value'] * t[1]), axis=1)
+
+
+
